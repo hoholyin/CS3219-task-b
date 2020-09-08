7a73cd3e646c2e4a01f16bbe186a0621b4b3f227
hoholyin <holyin97@gmail.com>
2020-09-08 18:06:52 +0800

    update process env

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const localConnection = "mongodb://localhost/cs3219-task-b1"
 const connection = "mongodb+srv://holyin:cs3219-task-b1@cluster0.luoag.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 function connect() {
-    const isLocal = process.env.PATH.includes('hoholyin');
+    const isLocal = process.env.USER.includes('hoholyin');
     console.log('isLocal: ' + isLocal);
     console.log(process.env);
     const uri = isLocal ? localConnection : connection;
